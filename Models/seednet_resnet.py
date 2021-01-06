c76d44eb40972aaebfe483ad0dab6e701e6956df
Xiaohan Chen <xiaohan.chen@utexas.edu>
2021-01-06 13:12:37 -0600

    Fixed padding arg

--- Models/seednet_resnet.py
+++ Models/seednet_resnet.py
@@ -32,10 +32,10 @@ class BasicBlock(nn.Module):
     def __init__(self, in_planes, planes, stride=1):
         super(BasicBlock, self).__init__()
         # self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.conv1 = conv3x3(in_planes, planes, stride=stride, padding=1)
+        self.conv1 = conv3x3(in_planes, planes, stride=stride)  #, padding=1)
         self.bn1 = nn.BatchNorm2d(planes)
         # self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.conv2 = conv3x3(planes, planes, stride=1, padding=1)
+        self.conv2 = conv3x3(planes, planes, stride=1)  #, padding=1)
         self.bn2 = nn.BatchNorm2d(planes)
 
         self.shortcut = nn.Sequential()
@@ -63,7 +63,7 @@ class Bottleneck(nn.Module):
         self.conv1 = conv1x1(in_planes, planes)
         self.bn1 = nn.BatchNorm2d(planes)
         # self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.conv2 = conv3x3(planes, planes, stride=stride, padding=1)
+        self.conv2 = conv3x3(planes, planes, stride=stride)  #, padding=1)
         self.bn2 = nn.BatchNorm2d(planes)
         # self.conv3 = nn.Conv2d(planes, self.expansion*planes, kernel_size=1, bias=False)
         self.conv3 = conv1x1(planes, self.expansion*planes)
@@ -92,7 +92,7 @@ class ResNet(nn.Module):
         self.in_planes = 64
 
         # self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
-        self.conv1 = conv3x3(3, 64, stride=1, padding=1)
+        self.conv1 = conv3x3(3, 64, stride=1)  #, padding=1)
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
